243db901bbc8942af5d496e7a701b0a2997ff9fe
FreaktheBear <freekdenbeer@hotmail.com>
2025-04-19 22:30:52 +1200

    Added class hadec2altaz.py

---
+++ astrolibpico/hadec2altaz.py
@@ -0,0 +1,35 @@
+# Created by: Freek den Beer
+# Created on: 2025-04-19
+# Description: This class converts hour angle and declination to alt-azimuth coordinates.
+# This class is adopted from the astrolibR library and converted for MicroPython.
+# License: MIT
+
+from math import sin, cos, atan2, pi, sqrt
+    
+class HaDec2AltAz:
+
+    def __init__(self, ha=0, dec=0, lat=0, ws=False):
+        self.ha = ha
+        self.dec = dec
+        self.lat = lat
+        self.ws = ws
+    
+    def convert_hadec2altaz(self):
+        deg2rad = pi/180
+        sh = sin(self.ha*deg2rad)
+        ch = cos(self.ha*deg2rad)
+        sd = sin(self.dec*deg2rad)
+        cd = cos(self.dec*deg2rad)
+        sl = sin(self.lat*deg2rad)
+        cl = cos(self.lat*deg2rad)
+        x = - ch * cd * sl + sd * cl
+        y = - sh * cd
+        z = ch * cd * cl + sd * sl
+        r = sqrt(x^2 + y^2)
+        alt = atan2(z,r) / deg2rad
+        az = atan2(y,x) / deg2rad
+        if az < 0:
+            az = az + 360
+        if self.ws == True:
+            az = (az + 180) % 360
+        return(alt, az)
